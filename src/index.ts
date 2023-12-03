59fdb6c153667fbffd01fe6f624d655029ccef20
yusufstawan <yusufsetiyawan45@gmail.com>
2023-12-03 14:46:39 +0700

    Initialize project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({
+    status: "200",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
